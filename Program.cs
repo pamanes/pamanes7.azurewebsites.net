10bd83923a39a908fe4d3673f620aa864cfe83bc
pamanes <pamanes@gmail.com>
2025-07-03 21:27:33 -0700

    adds app insights

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
 using Markdig;
 using Markdig.Extensions.AutoIdentifiers;
-using Markdig.Extensions.AutoLinks;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
 
 
 namespace Blog
@@ -17,10 +15,12 @@ namespace Blog
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            
+            builder.Services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions()
+            {
+                ConnectionString = builder.Configuration["ApplicationInsights:ConnectionString"]
+            });
             //dotnet ef migrations add InitBlogSchema
             //dotnet ef database update
-            //builder.Services.AddDbContext<MDBlogDbContext>(options => options.UseSqlite("Data Source=md.db"));
             builder.Services.AddDbContext<MDBlogDbContext>(options => options.UseSqlite("Data Source=md.db")
                 //.LogTo(Console.WriteLine, LogLevel.Information)
                 //.EnableSensitiveDataLogging()
