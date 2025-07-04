bc195d82a12a9015a24820284a4082381e19a52c
pamanes <pamanes@gmail.com>
2025-07-03 21:41:46 -0700

    adds traces for app settings in admin login

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -6,16 +6,19 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using System;
+using Microsoft.ApplicationInsights;
 
 namespace Blog.Controllers
 {
     public class AdminController : Controller
     {
         private readonly AdminAuthSettings _authSettings;
+        private readonly TelemetryClient _telemetryClient;
 
-        public AdminController(IOptions<AdminAuthSettings> authOptions)
+        public AdminController(IOptions<AdminAuthSettings> authOptions, TelemetryClient telemetryClient)
         {
             _authSettings = authOptions.Value;
+            _telemetryClient = telemetryClient;
         }
 
         [HttpGet]
@@ -25,6 +28,18 @@ namespace Blog.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(string username, string password)
         {
+            if (string.IsNullOrEmpty(_authSettings.Username))
+            {
+                Exception ex = new Exception("Admin username is missing from configuration.");
+                _telemetryClient.TrackException(ex);
+                throw ex;
+            }
+            if (string.IsNullOrEmpty(_authSettings.PasswordHash))
+            {
+                Exception ex = new Exception("Admin password hash is missing from configuration.");
+                _telemetryClient.TrackException(ex);
+                throw ex;
+            }
             if (username == _authSettings.Username && BCrypt.Net.BCrypt.Verify(password, _authSettings.PasswordHash))
             {
                 var claims = new List<Claim>
